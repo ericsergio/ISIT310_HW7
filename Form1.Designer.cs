1323f5f916aef73bb43d3bde5868040d909a4afd
Eric Sergio <eric.sergio@bellevuecollege.edu>
2023-02-24 22:37:08 -0800

    HW7 Complete

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -41,10 +41,8 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.grid = new System.Windows.Forms.DataGridView();
-            this.pBox = new System.Windows.Forms.TextBox();
-            this.sBox = new System.Windows.Forms.TextBox();
+            this.tbox = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.supplierBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.partBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
@@ -55,10 +53,9 @@
             this.supplierCmb.DataSource = this.supplierBindingSource;
             this.supplierCmb.DisplayMember = "CompanyName";
             this.supplierCmb.FormattingEnabled = true;
-            this.supplierCmb.Location = new System.Drawing.Point(1104, 255);
-            this.supplierCmb.Margin = new System.Windows.Forms.Padding(6);
+            this.supplierCmb.Location = new System.Drawing.Point(673, 133);
             this.supplierCmb.Name = "supplierCmb";
-            this.supplierCmb.Size = new System.Drawing.Size(238, 33);
+            this.supplierCmb.Size = new System.Drawing.Size(121, 21);
             this.supplierCmb.TabIndex = 0;
             this.supplierCmb.ValueMember = "SupplierID";
             // 
@@ -71,10 +68,9 @@
             this.partCmb.DataSource = this.partBindingSource;
             this.partCmb.DisplayMember = "PartName";
             this.partCmb.FormattingEnabled = true;
-            this.partCmb.Location = new System.Drawing.Point(1104, 362);
-            this.partCmb.Margin = new System.Windows.Forms.Padding(6);
+            this.partCmb.Location = new System.Drawing.Point(673, 188);
             this.partCmb.Name = "partCmb";
-            this.partCmb.Size = new System.Drawing.Size(238, 33);
+            this.partCmb.Size = new System.Drawing.Size(121, 21);
             this.partCmb.TabIndex = 1;
             this.partCmb.ValueMember = "PartID";
             // 
@@ -88,10 +84,9 @@
             this.orderBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.orderBtn.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.orderBtn.ForeColor = System.Drawing.Color.White;
-            this.orderBtn.Location = new System.Drawing.Point(1104, 541);
-            this.orderBtn.Margin = new System.Windows.Forms.Padding(6);
+            this.orderBtn.Location = new System.Drawing.Point(673, 281);
             this.orderBtn.Name = "orderBtn";
-            this.orderBtn.Size = new System.Drawing.Size(242, 69);
+            this.orderBtn.Size = new System.Drawing.Size(121, 36);
             this.orderBtn.TabIndex = 2;
             this.orderBtn.Text = "Order";
             this.orderBtn.UseVisualStyleBackColor = false;
@@ -99,28 +94,25 @@
             // 
             // quantityInp
             // 
-            this.quantityInp.Location = new System.Drawing.Point(1108, 472);
-            this.quantityInp.Margin = new System.Windows.Forms.Padding(6);
+            this.quantityInp.Location = new System.Drawing.Point(675, 245);
             this.quantityInp.Name = "quantityInp";
-            this.quantityInp.Size = new System.Drawing.Size(94, 31);
+            this.quantityInp.Size = new System.Drawing.Size(49, 20);
             this.quantityInp.TabIndex = 3;
             // 
             // idInp
             // 
-            this.idInp.Location = new System.Drawing.Point(1117, 680);
-            this.idInp.Margin = new System.Windows.Forms.Padding(6);
+            this.idInp.Location = new System.Drawing.Point(679, 354);
             this.idInp.Name = "idInp";
-            this.idInp.Size = new System.Drawing.Size(94, 31);
+            this.idInp.Size = new System.Drawing.Size(49, 20);
             this.idInp.TabIndex = 4;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(1094, 203);
-            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Location = new System.Drawing.Point(668, 106);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(151, 35);
+            this.label1.Size = new System.Drawing.Size(76, 18);
             this.label1.TabIndex = 5;
             this.label1.Text = "Suppliers";
             // 
@@ -128,10 +120,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(1098, 315);
-            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Location = new System.Drawing.Point(670, 164);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(164, 35);
+            this.label2.Size = new System.Drawing.Size(81, 18);
             this.label2.TabIndex = 6;
             this.label2.Text = "Part Name";
             // 
@@ -139,10 +130,9 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(1101, 423);
-            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Location = new System.Drawing.Point(671, 220);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(135, 35);
+            this.label3.Size = new System.Drawing.Size(67, 18);
             this.label3.TabIndex = 7;
             this.label3.Text = "Quantity";
             // 
@@ -150,70 +140,47 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(1102, 639);
-            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Location = new System.Drawing.Point(672, 332);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(127, 35);
+            this.label4.Size = new System.Drawing.Size(65, 18);
             this.label4.TabIndex = 8;
             this.label4.Text = "OrderID";
             // 
             // grid
             // 
             this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.grid.Location = new System.Drawing.Point(55, 203);
-            this.grid.Margin = new System.Windows.Forms.Padding(6);
+            this.grid.Location = new System.Drawing.Point(28, 106);
             this.grid.Name = "grid";
             this.grid.RowHeadersWidth = 82;
-            this.grid.Size = new System.Drawing.Size(990, 657);
+            this.grid.Size = new System.Drawing.Size(634, 342);
             this.grid.TabIndex = 9;
             // 
-            // pBox
+            // tbox
             // 
-            this.pBox.Location = new System.Drawing.Point(323, 75);
-            this.pBox.Name = "pBox";
-            this.pBox.Size = new System.Drawing.Size(215, 31);
-            this.pBox.TabIndex = 10;
-            // 
-            // sBox
-            // 
-            this.sBox.Location = new System.Drawing.Point(74, 73);
-            this.sBox.Name = "sBox";
-            this.sBox.Size = new System.Drawing.Size(215, 31);
-            this.sBox.TabIndex = 11;
+            this.tbox.Location = new System.Drawing.Point(263, 55);
+            this.tbox.Margin = new System.Windows.Forms.Padding(2);
+            this.tbox.Name = "tbox";
+            this.tbox.Size = new System.Drawing.Size(110, 20);
+            this.tbox.TabIndex = 11;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(68, 22);
-            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label5.Location = new System.Drawing.Point(260, 28);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(118, 35);
+            this.label5.Size = new System.Drawing.Size(35, 18);
             this.label5.TabIndex = 12;
-            this.label5.Text = "sQuery";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(317, 24);
-            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(119, 35);
-            this.label6.TabIndex = 13;
-            this.label6.Text = "pQuery";
-            this.label6.Click += new System.EventHandler(this.label6_Click);
+            this.label5.Text = "test";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Teal;
-            this.ClientSize = new System.Drawing.Size(1989, 1181);
-            this.Controls.Add(this.label6);
+            this.ClientSize = new System.Drawing.Size(962, 552);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.sBox);
-            this.Controls.Add(this.pBox);
+            this.Controls.Add(this.tbox);
             this.Controls.Add(this.grid);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -224,7 +191,6 @@
             this.Controls.Add(this.orderBtn);
             this.Controls.Add(this.partCmb);
             this.Controls.Add(this.supplierCmb);
-            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -250,10 +216,8 @@
         private System.Windows.Forms.DataGridView grid;
     private System.Windows.Forms.BindingSource supplierBindingSource;
     private System.Windows.Forms.BindingSource partBindingSource;
-    private System.Windows.Forms.TextBox pBox;
-    private System.Windows.Forms.TextBox sBox;
+    private System.Windows.Forms.TextBox tbox;
     private System.Windows.Forms.Label label5;
-    private System.Windows.Forms.Label label6;
   }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,7 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
+using System.Data.Linq;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -11,109 +14,77 @@ namespace Homework7
 {
   public partial class Form1 : Form
   {
-    OrderModelContainer1 context = new OrderModelContainer1();
-    public Form1()
-    {
-      InitializeComponent();
-    }
+    OrderModelContainer1 context = new OrderModelContainer1();       
+        public Form1()    
+        {      
+            InitializeComponent();    
+        }
 
-    private void Form1_Load(object sender, EventArgs e)
-    {
-      //supplierCmb, partCmb, quantityInp, idInp, grid 
-      Populate();
-
-    }
-
-
-
-    private void orderBtn_Click(object sender, EventArgs e)
-    {
-      InsertOrder();     
-    }
-
-    private void Populate()
-    {
-      //grid
-      var gridQuery = from eachOrder in context.Orders
+        private void Form1_Load(object sender, EventArgs e)    
+        {
+        //supplierCmb, partCmb, quantityInp, idInp, grid       
+            Populate();    
+        }    
+        private void orderBtn_Click(object sender, EventArgs e)    
+        {      
+            InsertOrder();    
+        }
+    
+        private void Populate()    
+        {      
+            //grid      
+            var gridQuery = from eachOrder in context.Orders
                       select new
                       {
-                        id = eachOrder.OrderID,
-                        name = eachOrder.Supplier.CompanyName,
-                        Phone = eachOrder.Supplier.PhoneNumber,
-                        Date = eachOrder.OrderDate,                        
-                        Quantity = eachOrder.Quantity,
-                        Part = eachOrder.Part.PartName
+                        ID = eachOrder.OrderID,
+                        NAME = eachOrder.Supplier.CompanyName,
+                        PHONE = eachOrder.Supplier.PhoneNumber,
+                        DATE = eachOrder.OrderDate,                        
+                        QUANTITY = eachOrder.Quantity,
+                        PART = eachOrder.Part.PartName
                       };
-      grid.DataSource = gridQuery.ToList();
-
-      var suppComboQuery = from eachSupplier in context.Suppliers
-                           select new
-                           {
-                             //id = eachSupplier.SupplierID,
-                             eachSupplier.CompanyName,
-                             //PhoneNumber = eachSupplier.PhoneNumber
-                           };
-
-
-
-
-      supplierCmb.DataSource= suppComboQuery.ToList();
-
-
-      //partCmb
-      var partComboQuery = from eachPart in context.Parts
-                           select new
-                           {
-                             id = eachPart.PartID,
-                             eachPart.PartName,
-                             price = eachPart.Price
-                           };
-      partCmb.DataSource = partComboQuery.ToList();
-
-
-      //quantityInp
-
-
-
-      //idInp
-      var orderId = from eachPart in context.Orders
-                    select eachPart.OrderID;
-
-      /*{
-          id = eachPart.OrderID,
-          date = DateTime.Now//eachPart.OrderDate,
-          qty = eachPart.Quantity,
-          part_id = eachPart.PartID,
-          supp_id = eachPart.SupplierID
-      };*/
-      //idInp.DataSource = orderIdInput;
-      grid.AutoResizeColumns();
-      grid.AlternatingRowsDefaultCellStyle.BackColor = Color.LightSkyBlue;
-
-    }
-
-    private void InsertOrder()
-    {
-      var sQuery = (from eachOrder in context.Orders
-                    where supplierCmb.Text == eachOrder.Supplier.CompanyName
-                    select eachOrder.SupplierID).First();
-
       
+            grid.DataSource = gridQuery.ToList();
 
-      var pQuery = (from eachOrder in context.Orders
-                    where partCmb.Text == eachOrder.Part.PartName
-                    //select eachOrder.PartID).FirstOrDefault();
-                    select eachOrder.Part.PartID).First();
+            supplierCmb.DataSource = context.Suppliers.ToList();
+            supplierCmb.DisplayMember = "CompanyName";
+            supplierCmb.ValueMember = "supplierID";
+            
+            partCmb.DataSource = context.Parts.ToList();
+            partCmb.DisplayMember = "PartName";
+            partCmb.ValueMember = "PartID";
+      
+            grid.AutoResizeColumns();      
+            grid.AlternatingRowsDefaultCellStyle.BackColor = Color.LightSkyBlue;            
+        }
+        private void GetIdValue()
+        {
+            var r = grid.Rows.Count;
+            //var d = grid.Rows[r];
+            var s = r.ToString();
+            var t = int.Parse(s) - 1;
+            var w = grid.Rows[t].Cells[0].Value.ToString();
+            idInp.Text = w.ToString();
+        }
 
-      pBox.Text = pQuery.ToString();
-      sBox.Text = sQuery.ToString();
+        private void InsertOrder()    
+        {      
+            var sQuery = (int)supplierCmb.SelectedValue;
+            var pQuery = (int)(partCmb.SelectedValue);
 
-      var newOrder = new Order { OrderDate= DateTime.Now, Quantity = int.Parse(quantityInp.Text), SupplierID = sQuery, PartID = pQuery };
-      context.Orders.Add(newOrder);
-      context.SaveChanges();
-      Populate();
+            var newOrder = new Order 
+            { 
+                OrderDate= DateTime.Now, 
+                Quantity = int.Parse(quantityInp.Text), 
+                SupplierID = sQuery, 
+                PartID = pQuery 
+            };
+            context.Orders.Add(newOrder);      
+            context.SaveChanges();      
+            Populate();
+            GetIdValue();
+        }
 
-    }
 
     private void txt_TextChanged(object sender, EventArgs e)
     {
